71deca3fdef6cb054ecefb07277d1c13836f35a1
Carla220274 <crcleite@gmail.com>
2025-12-03 16:56:13 +0100

    Update PLUGIN.cs

--- PLUGIN.cs
+++ PLUGIN.cs
@@ -1,17 +1,20 @@
 ﻿using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.Runtime;
 using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.ApplicationServices.Core;
 
 namespace CAD_AI_PLUGIN
 {
-    public class Plugin
+    public class Plugin   // ❌ REMOVED MarshalByRefObject (GitHub cannot compile it)
     {
         [CommandMethod("HELLOAI")]
         public static void HelloAI()
         {
-            Document doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            Document doc = Application.DocumentManager.MdiActiveDocument;
             Editor ed = doc.Editor;
             ed.WriteMessage("\n🧚‍♀️ Hello Carla, your AI plugin is ALIVE in AutoCAD!");
         }
     }
 }
+
+
